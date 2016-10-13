404d60513e4d5424ce9e65bb16fbda6a9f0d853d
copy4dev <copy4dev@126.com>
2016-10-13 11:03:42 +0800

    orign

--- src/main/java/com/thinkgem/jeesite/modules/timer/TimerJob.java
+++ src/main/java/com/thinkgem/jeesite/modules/timer/TimerJob.java
@@ -3,8 +3,6 @@ package com.thinkgem.jeesite.modules.timer;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.thinkgem.jeesite.modules.debug.util.AddressUtil;
-
 /**
  * 定时任务示例
  * 
@@ -17,8 +15,6 @@ public class TimerJob {
 
 	@Scheduled(cron = "*/10 * * * * *")
 	public void test() {
-		System.out.println(" --- start --- ");
-		AddressUtil.main(null);
-		System.out.println(" ---  end  --- ");
+
 	}
 }
